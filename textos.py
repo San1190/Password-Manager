e6b616d1b7131f61e82e58b668639f71eb994989
San1190 <77977604+San1190@users.noreply.github.com>
2023-11-15 16:15:40 +0100

    Update textos.py

--- textos.py
+++ textos.py
@@ -16,99 +16,98 @@ class bcolors:
     ORANGE = '\033[33m'
 
 def Registro(): #debe tener registrado un usuario con una contraseña para poder acceder
-    os.system("cls")
-    print(bcolors.RED + bcolors.BOLD)
-    tprint("Password Manager", font="small")
-    print(bcolors.GREEN + bcolors.BOLD)
-    tprint("By: San1190", font="small")
-    print(bcolors.ENDC)
-    print(bcolors.BOLD + bcolors.SKYBLUE)
-    print("¿Qué desea hacer?")
-    print(bcolors.ENDC)
-    print(bcolors.SKYBLUE)
-    print("1. Registrarse")
-    print("2. Iniciar sesión")
-    print("3. Salir")
-    opcion = int(input("Elija una opción: " + bcolors.ORANGE))
-    print(bcolors.SKYBLUE)
-    return opcion
-    
+   os.system("cls")
+   print(bcolors.RED + bcolors.BOLD)
+   tprint("Password Manager", font="small")
+   print(bcolors.GREEN + bcolors.BOLD)
+   tprint("By: San1190", font="small")
+   print(bcolors.ENDC)
+   print(bcolors.BOLD + bcolors.SKYBLUE)
+   print("What do you want to do?")
+   print(bcolors.ENDC)
+   print(bcolors.SKYBLUE)
+   print("1. Register")
+   print("2. Login")
+   print("3. Exit")
+   opcion = int(input("Choose an option: " + bcolors.ORANGE))
+   print(bcolors.SKYBLUE)
+   return opcion
+   
 
 def Inicio():
-    os.system("cls")
-    print(bcolors.RED + bcolors.BOLD)
-    tprint("Sesion iniciada", font="small")
-    print(bcolors.ENDC)
-    print(bcolors.BOLD + bcolors.SKYBLUE)
-    print("¿Qué desea hacer? \n")
-    print(bcolors.ENDC)
-    print(bcolors.SKYBLUE)
-    print("1. Crear contraseña")
-    print("2. Leer contraseña")
-    print("3. Cerrar sesión")
-    print("4. Borrar usuario")
-    print("5. Borra contraseña")
-    print("6. Salir")
-    opcion = int(input("Elija una opción: " + bcolors.ORANGE))
-    print(bcolors.SKYBLUE)
-    return opcion
+   os.system("cls")
+   print(bcolors.RED + bcolors.BOLD)
+   tprint("Session started", font="small")
+   print(bcolors.ENDC)
+   print(bcolors.BOLD + bcolors.SKYBLUE)
+   print("What do you want to do? \n")
+   print(bcolors.ENDC)
+   print(bcolors.SKYBLUE)
+   print("1. Create password")
+   print("2. Read password")
+   print("3. Logout")
+   print("4. Delete user")
+   print("5. Delete password")
+   print("6. Exit")
+   opcion = int(input("Choose an option: " + bcolors.ORANGE))
+   print(bcolors.SKYBLUE)
+   return opcion
 
 
 def Error(num_Error):
-    os.system("cls")
-    print(bcolors.RED)
-    if num_Error == 1:
-        print("No ha introducido un número\n")
-        input("Pulse enter para continuar...")
-        os.system("cls")
-        
-    elif num_Error == 2:
-        print("No se ha podido crear el fichero\n")
-        input("Pulse enter para continuar...")
-        os.system("cls")
-    elif num_Error == 3:
-        print("No se ha podido escribir en el fichero\n")
-        input("Pulse enter para continuar...")
-        os.system("cls")
-    elif num_Error == 4:
-        print("El usuario ya existe\n")
-        input("Pulse enter para continuar...")
-        os.system("cls")
-    elif num_Error == 5:
-        print("La contraseña o el usuario son incorrectos\n")
-        input("Pulse enter para continuar...")
-        os.system("cls")
-    elif num_Error == 6:
-        
-        print("No se ha podido registrar el usuario\n")
-        input("Pulse enter para continuar...")
-        os.system("cls")
-    elif num_Error == 7:
-        print("No se ha podido borrar el fichero\n")
-        input("Pulse enter para continuar...")
-        os.system("cls")
+   os.system("cls")
+   print(bcolors.RED)
+   if num_Error == 1:
+       print("You have not entered a number\n")
+       input("Press enter to continue...")
+       os.system("cls")
+       
+   elif num_Error == 2:
+       print("Could not create the file\n")
+       input("Press enter to continue...")
+       os.system("cls")
+   elif num_Error == 3:
+       print("Could not write to the file\n")
+       input("Press enter to continue...")
+       os.system("cls")
+   elif num_Error == 4:
+       print("The user already exists\n")
+       input("Press enter to continue...")
+       os.system("cls")
+   elif num_Error == 5:
+       print("The password or the user is incorrect\n")
+       input("Press enter to continue...")
+       os.system("cls")
+   elif num_Error == 6:
+       print("Could not register the user\n")
+       input("Press enter to continue...")
+       os.system("cls")
+   elif num_Error == 7:
+       print("Could not delete the file\n")
+       input("Press enter to continue...")
+       os.system("cls")
 
-    print(bcolors.ENDC)
-        
+   print(bcolors.ENDC)
+       
 
 def Despedida():
-    os.system("cls")
-    print(bcolors.ORANGE + bcolors.BOLD)
-    print("Gracias por usar la aplicación")
-    exit()
+   os.system("cls")
+   print(bcolors.ORANGE + bcolors.BOLD)
+   print("Thank you for using the application")
+   exit()
 
 def CerrarSesion():
-    os.system("cls")
-    print("Sesión cerrada con éxito")
-    input("Pulse enter para continuar...")
-    os.system("cls")
+   os.system("cls")
+   print("Session closed successfully")
+   input("Press enter to continue...")
+   os.system("cls")
 
 def RegistroCorrecto(usuario, contraseña):
-    os.system("cls")
-    print(bcolors.SKYBLUE)
-    print("Usuario registrado con éxito")
-    print("Su usuario es: " + usuario)
-    print("Su contraseña es: " + contraseña)
-    print("Guarde estos datos en un lugar seguro")
-    input("Pulse enter para continuar...")
-    os.system("cls")
+   os.system("cls")
+   print(bcolors.SKYBLUE)
+   print("User registered successfully")
+   print("Your username is: " + usuario)
+   print("Your password is: " + contraseña)
+   print("Keep these data in a safe place")
+   input("Press enter to continue...")
+   os.system("cls")
