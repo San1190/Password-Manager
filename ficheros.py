5dd58476ac4ca9c40df7f330202c1a9d4f9af883
San1190 <77977604+San1190@users.noreply.github.com>
2023-11-15 16:16:00 +0100

    Update ficheros.py

--- ficheros.py
+++ ficheros.py
@@ -68,75 +68,72 @@ def leer_contrasena_usuario(nombre):
     
 
 
-
+# ...
 
 def borrar_fichero(numero, usuario):
+  path_usuario = os.path.join("Usuarios", usuario)
+  
+  # Verificar si el directorio existe
+  if not os.path.exists(path_usuario):
+      print(f"The directory {path_usuario} does not exist.")
+      return
+
+  ficheros = os.listdir(path_usuario)
+  if "contraseña_usuario.txt" in ficheros:
+      ficheros.remove("contraseña_usuario.txt")
+  
+  # Verificar si el índice es válido
+  if numero - 1 >= len(ficheros):
+      print(f"The index {numero} is not valid. There is no file at that position.")
+      return
+
+  # Imprimir el archivo a eliminar
+  
+  # Intentar eliminar el archivo y manejar cualquier error que pueda ocurrir
+  try:
+      os.remove(os.path.join(path_usuario, ficheros[numero - 1]))
+  except OSError as error:
+      print(f"Error deleting the file: {error}")
+
+# ...
+
+def borrar_directorio(usuario):
    path_usuario = os.path.join("Usuarios", usuario)
    
-   # Verificar si el directorio existe
-   if not os.path.exists(path_usuario):
-       print(f"El directorio {path_usuario} no existe.")
-       return
+   try:
+       # Eliminar el directorio y su contenido recursivamente
+       shutil.rmtree(path_usuario)
+       print(f"Directory '{path_usuario}' successfully deleted.")
+   except FileNotFoundError:
+       print(f"The directory '{path_usuario}' does not exist.")
+   except Exception as e:
+       print(f"Error trying to delete the directory '{path_usuario}': {e}")
 
+# ...
+
+def crear_directorio(usuario):
+   path_contraseñas = os.path.join("Usuarios", usuario)
+   if not os.path.exists(path_contraseñas):
+       os.makedirs(path_contraseñas)
+       crear_fichero_usuario(usuario)
+       print("Password folder successfully created.")
+       return True
+   
+   
+       
+   else:
+       return False
+       Error(4)
+
+# ...
+
+def mostrarNombres(usuario):
+   path_usuario = os.path.join("Usuarios", usuario)
    ficheros = os.listdir(path_usuario)
    if "contraseña_usuario.txt" in ficheros:
        ficheros.remove("contraseña_usuario.txt")
+   for i in range(len(ficheros)):
+       ficheros[i] = ficheros[i].replace(".txt", "")
+       print(f"{i + 1}." + ficheros[i])
    
-   # Verificar si el índice es válido
-   if numero - 1 >= len(ficheros):
-       print(f"El índice {numero} no es válido. No hay un archivo en esa posición.")
-       return
-
-   # Imprimir el archivo a eliminar
-   print(os.path.join(path_usuario, ficheros[numero - 1]))
-   
-   # Intentar eliminar el archivo y manejar cualquier error que pueda ocurrir
-   try:
-       os.remove(os.path.join(path_usuario, ficheros[numero - 1]))
-   except OSError as error:
-       print(f"Error al eliminar el archivo: {error}")
-
-
-# Función para crear un directorio de usuario
-def crear_directorio(usuario):
-    path_contraseñas = os.path.join("Usuarios", usuario)
-    if not os.path.exists(path_contraseñas):
-        os.makedirs(path_contraseñas)
-        crear_fichero_usuario(usuario)
-        print("Se ha creado la carpeta contraseñas")
-        return True
-    
-    
-        
-    else:
-        return False
-        Error(4)
-
-
-
-
-
-def borrar_directorio(usuario):
-    path_usuario = os.path.join("Usuarios", usuario)
-    
-    try:
-        # Eliminar el directorio y su contenido recursivamente
-        shutil.rmtree(path_usuario)
-        print(f"Directorio '{path_usuario}' eliminado con éxito.")
-    except FileNotFoundError:
-        print(f"El directorio '{path_usuario}' no existe.")
-    except Exception as e:
-        print(f"Error al intentar eliminar el directorio '{path_usuario}': {e}")
-
-
-
-def mostrarNombres(usuario):
-    path_usuario = os.path.join("Usuarios", usuario)
-    ficheros = os.listdir(path_usuario)
-    if "contraseña_usuario.txt" in ficheros:
-        ficheros.remove("contraseña_usuario.txt")
-    for i in range(len(ficheros)):
-        ficheros[i] = ficheros[i].replace(".txt", "")
-        print(f"{i + 1}." + ficheros[i])
-    
-    return ficheros
+   return ficheros 
